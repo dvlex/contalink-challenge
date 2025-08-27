4874abde146ed302b6c8cb49cd0aeeef16e851fb
Alex Castillo <alex@lexdrel.com>
2025-08-27 00:15:12 +0000

    fix cache update strategy

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,6 +1,8 @@
 class InvoicesController < ApplicationController
   # GET /invoices or /invoices.json
   def index
+      # TODO: it's recommendable to have the timestamps, since, they updated_at is the best way to know if the data is fresh
+      # TODO: add a cache versioning strategy that uses filter params
       cache_key = "invoices:index:v#{Invoice.maximum(:invoice_date).to_i}"
       @cached_json = Rails.cache.fetch(cache_key, expires_in: 10.minutes) do
         @invoices = Invoice.all
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,14 @@
 class Invoice < ApplicationRecord
+  before_commit :clear_invoice_cache
+  before_destroy :clear_invoice_cache
+
+  def delete
+    raise "Don't use DELETE, since it doesn't use transactions neither callbacks, use 'destroy' instead"
+  end
+
+  private
+
+  def clear_invoice_cache
+    Rails.cache.delete_matched("invoices:index:*")
+  end
 end
